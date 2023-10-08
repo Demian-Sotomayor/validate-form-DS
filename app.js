8b8b9aa7741b552892b2972aef7fad606ac8742d
Demian Sotomayor <demian.sotomayor.ur@gmail.com>
2023-10-08 12:41:53 -0300

    Cambiando código para validar formulario

--- app.js
+++ app.js
@@ -1,34 +1,62 @@
+// ________ Variables ________
 
 const form = document.querySelector('.needs-validation');
 const submitBtn = document.querySelector('.btn-send');
 const alert = document.querySelector('.alert');
-const inputAlert = document.querySelector('input')
+const inputs = form.querySelectorAll('input, select, textarea');
 
 submitBtn.addEventListener('click', e => {
 
   e.preventDefault();
 
-  // Validamos que los campos
-  if(form.checkValidity() === false) {
+  // ---- Validar formulario ----
+  if(!form.checkValidity()) {
 
-    // Añade clases para mostrar alertas
-    form.classList.add('was-validated');
+    // ---- Recorrer inputs ----
+    inputs.forEach(input => {
+
+      if(!input.checkValidity()) {
+
+        input.classList.add('alert-danger');
+        
+        // Quitar alerta después de 4 segundos
+        setTimeout(() => {
+          input.classList.remove('alert-danger');
+        }, 4000);
+
+      }   
+    });
+    
+    // Mostrar alerta
     alert.classList.remove('d-none');
     
-    // Remueve las clases pasado 4 segundos
+    // Ocultar alerta tras 4 segundos
     setTimeout(() => {
       alert.classList.add('d-none');
     }, 4000);
-    return;
-  }
 
-  // Enviar el formulario
-  enviarForm();
+    form.classList.add('was-validated');
+
+  } else {
+
+    // Si está validado, ir hacia la función enviarForm
+    enviarForm();
+
+  }
 
 });
 
+// Función para enviar
 function enviarForm() {
-    form.reset();
+  // Limpia los campos
+  form.reset();
 
-    window.alert("Enviando formulario!")
+  // Añade un mensaje en la alerta
+  alert.innerText = 'Enviando formulario...';
+  alert.classList.remove('d-none');
+
+  // Oculta la alerta
+  setTimeout(() => {
+    alert.classList.add('d-none');
+  }, 4000);
 }
